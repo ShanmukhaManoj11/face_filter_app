32d567a55dc26c3da166270f9aaa9479aa1dece8
ShanmukhaManoj11 <shanmukhamanoj007@gmail.com>
2019-09-06 00:44:13 -0400

    change pose of 3d object based on detection - in progress

--- src/3dLoader.cpp
+++ src/3dLoader.cpp
@@ -7,6 +7,12 @@
 #include <opencv2/viz/vizcore.hpp>
 
 int main(int argc,char** argv){
+	double camZ=1500.0;
+	cv::Vec3f camPos(0,0,(float)camZ);
+	cv::Vec3f camFocalPoint(0,0,0);
+	cv::Vec3f camYdir(0,0,0);
+	cv::Affine3f viewPose=cv::viz::makeCameraPose(camPos,camFocalPoint,camYdir);
+
 	cv::String obj("../models/PLY_Pasha.ply");
 	cv::viz::WMesh objmesh( cv::viz::Mesh::load(obj) ); //3d object mesh as widget on viz3d window
 
@@ -24,18 +30,40 @@ int main(int argc,char** argv){
 	window3d.showWidget("object",objmesh);
 	window3d.showWidget("frame",wframe);
 	window3d.setFullScreen(true);
+	window3d.setViewerPose(viewPose);
 
 	std::shared_ptr<FaceDetector> _faceDetector(new FaceDetector());
 
 	while(!window3d.wasStopped()){
 		videoHandle>>frame;
 		if(frame.empty()) break;
+
 		_faceDetector->detectFaces(frame);
 		_faceDetector->detectFacialLandmarks();
 		frame=_faceDetector->getProcessedFrame();
-		
 		wframe.setImage(frame);
 		wframe.setPose(framePose);
+
+		std::vector<cv::Rect> _faces=_faceDetector->getDetectedFaces();
+		if(!_faces.empty()){
+			double cx=_faces[0].x;
+			double cy=_faces[0].y;
+			/*
+			cv::Point3d origin;
+			cv::Vec3d direction;
+			window3d.converTo3DRay(cv::Point3d(cx,cy,0),origin,direction);
+			//std::cout<<direction[0]<<" "<<direction[1]<<" "<<direction[2]<<std::endl;
+			if(direction[2]!=0){
+				double worldx=(direction[0]/direction[2])*camZ;
+				double worldy=(direction[1]/direction[2])*camZ;
+				cv::Affine3f meshPose(cv::Vec3f(0,0,0),cv::Vec3f(worldx,worldy,0));
+				objmesh.setPose(meshPose);
+			}
+			*/
+			cv::Affine3f meshPose(cv::Vec3f(0,0,0),cv::Vec3f(cx,cy,50));
+			objmesh.setPose(meshPose);
+		}
+
 		window3d.spinOnce(1,true);
 	}
 }
