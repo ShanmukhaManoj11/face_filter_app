d99db7aea46825591633df96f46fd60a894e3eda
ShanmukhaManoj11 <shanmukhamanoj007@gmail.com>
2019-09-05 22:23:04 -0400

    3d object on 2d image as widgets in viz3d window

--- src/3dLoader.cpp
+++ src/3dLoader.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -16,7 +17,26 @@
 
 int main(int argc,char** argv){
 	cv::String obj("../models/PLY_Pasha.ply");
-	cv::viz::Mesh mesh=cv::viz::readMesh(obj);
+	cv::viz::WMesh objmesh( cv::viz::Mesh::load(obj) ); //3d object mesh as widget on viz3d window
+
+	cv::VideoCapture videoHandle;
+	videoHandle.open(0);
+	int frameWidth=videoHandle.get(cv::CAP_PROP_FRAME_WIDTH);
+	int frameHeight=videoHandle.get(cv::CAP_PROP_FRAME_HEIGHT);
+	cv::Mat frame;
+	cv::Affine3f framePose(cv::Vec3f(M_PI,0,0),cv::Vec3f(0,0,0)); //set affine transformation to rotate frame on widget
+	videoHandle>>frame;
+	cv::viz::WImage3D wframe(frame,cv::Size2d(frameWidth,frameHeight)); //camera frame as widget on viz3d window
+
 	cv::viz::Viz3d window3d("3d model");
-	window3d.spin();
+	window3d.showWidget("object",objmesh);
+	window3d.showWidget("frame",wframe);
+
+	while(!window3d.wasStopped()){
+		videoHandle>>frame;
+		if(frame.empty()) break;
+		wframe.setImage(frame);
+		wframe.setPose(framePose);
+		window3d.spinOnce(1,true);
+	}
 }
